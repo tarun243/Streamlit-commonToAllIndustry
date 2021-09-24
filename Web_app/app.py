b77cefd382101c6e2ec9971f4cb433b6ba9036d9
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-09-24 19:07:37 +0530

    Update app.py

--- Web_app/app.py
+++ Web_app/app.py
@@ -2,10 +2,15 @@ import streamlit as vAR_st
 import streamlit.components.v1 as components
 import pandas as pd
 from IPython.display import HTML
-from sklearn.linear_model import LogisticRegression 
 import time
 import base64 
 timestr = time.strftime("%Y%m%d-%H%M%S")
+from sklearn.linear_model import LogisticRegression 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans
+
 
 #for Setting the page layout to wide
 vAR_st.set_page_config(layout="wide")
@@ -22,55 +27,65 @@ with vAR_title:
   vAR_st.write('')
   vAR_st.markdown("<h1 style='text-align: center; color: blue; font-size:29px;'>Powered by Streamlit and Google Cloud</h1>", unsafe_allow_html=True)
 
- #To customize the background colour of the submit button  
-m = vAR_st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: rgb(47 236 106) ;
-}
-</style>""", unsafe_allow_html=True)
 
 
-vAR_st.markdown("""
-<style>
-div.stButton > button:first-child { border: 1px solid; width: 55%; }
-</style>""", unsafe_allow_html=True)
-
-
-vAR_st.markdown("""
-<hr style="width:100%;height:3px;background-color:gray;border-width:10">
-""", unsafe_allow_html=True)
-
-
+#for background color of sidebar
 vAR_st.markdown("""<style>.css-17eq0hr {
     background-color: #4c85e4;
 }
 </style>""", unsafe_allow_html=True)
 
 
-def training():
-
-  #training data
-  training_data = df_training 
-  training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
-            
-  #feature selection for training
-  training_data_features = training_data_features[['Customer Lifetime(Days)']]
-
-  #Label for Training
-  training_data_label = training_data[['Churn']]
-
-  #model training 
-  model = LogisticRegression()
-  model_training = model.fit(training_data_features,training_data_label)
+#for clear/reset button
+vAR_st.markdown("""<style>#root > div:nth-child(1) > div > div > div > div > section.css-1lcbmhc.e1fqkh3o0 > div.css-17eq0hr.e1fqkh3o1 > div.block-container.css-1gx893w.eknhn3m2 > div:nth-child(1) > div:nth-child(4)  
+{
+    background-color:rgb(47 236 106);  
+    top: 200px; 
+    border: 1px solid; 
+    padding: 10px;}
+</style>""", unsafe_allow_html=True)
 
 
-def testing():
+#for clear/reset button
+vAR_st.markdown("""<style>p, ol, ul, dl {
+    margin: 0px 100px 1rem;
+    padding: 0px;
+    font-size: 1rem;
+    font-weight: 400;
+}
+</style>""", unsafe_allow_html=True)
+
+
+#To customize the background colour of the submit button  
+m = vAR_st.markdown("""
+<style>
+div.stButton > button:first-child {border: 1px solid; width: 55%;
+    background-color: rgb(47 236 106) ;
+}
+</style>""", unsafe_allow_html=True)
+
+
+#for .css file
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         vAR_st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+# local_css("style.css")
+
+
+#for horizontal line
+vAR_st.markdown("""
+<hr style="width:100%;height:3px;background-color:gray;border-width:10">
+""", unsafe_allow_html=True)
+
+
+
+
+def training(method):
 
   #training dataset
   training_data = df_training 
   training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
-            
+
   #feature selection for training
   training_data_features = training_data_features[['Customer Lifetime(Days)']]
 
@@ -78,7 +93,24 @@ def testing():
   training_data_label = training_data[['Churn']]
 
   #model training 
-  model = LogisticRegression()
+  model = method()
+  model_training = model.fit(training_data_features,training_data_label)
+
+
+def testing(method):
+
+  #training dataset
+  training_data = df_training 
+  training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
+
+  #feature selection for training
+  training_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+  #Label for Training
+  training_data_label = training_data[['Churn']]
+
+  #model training 
+  model = method()
   model_training = model.fit(training_data_features,training_data_label)
 
   #Test Dataset
@@ -107,7 +139,7 @@ def testing():
   vAR_st.write('')
 
 
-def test_code():
+def test_code_log():
   with vAR_st.echo():
     #training dataset
     training_data = df_training 
@@ -147,9 +179,7 @@ def test_code():
     vAR_st.write(table_7)
 
 
-
-
-def train_code():
+def train_code_log():
   with vAR_st.echo():
     def training():
 
@@ -167,7 +197,126 @@ def train_code():
       model = LogisticRegression()
       model_training = model.fit(training_data_features,training_data_label)
 
-def download():
+
+def test_code_ran():
+  with vAR_st.echo():
+    #training dataset
+    training_data = df_training 
+    training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
+            
+    #feature selection for training
+    training_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+    #Label for Training
+    training_data_label = training_data[['Churn']]
+
+    #model training 
+    model = RandomForestClassifier()
+    model_training = model.fit(training_data_features,training_data_label)
+
+    #Test Dataset
+    test_data = df_testing
+
+    #Feature Selection for Model Testing
+    test_data_features = test_data[['Customer Lifetime(Days)']]
+    test_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+    #Model Testing
+    model_testing = model.predict(test_data_features)
+    model_prediction = pd.DataFrame(model_testing)
+    model_prediction = pd.DataFrame(model_testing,columns=['Churn Prediction'])
+
+    prediction_result = test_data.merge(model_prediction,left_index=True,right_index=True)
+
+    #Getting the Probability of Churn
+    prediction_result_probability_all_features = model.predict_proba(test_data_features)
+    prediction_result_probability_all_features = pd.DataFrame(prediction_result_probability_all_features,
+      columns=['Probability of Non Churn', 'Probability of Churn'])
+    churn_probability = prediction_result.merge(prediction_result_probability_all_features,
+      left_index=True,right_index=True)
+    table_7 = HTML(churn_probability.to_html(col_space=None,max_rows=10,max_cols=6))
+    vAR_st.write(table_7)
+
+
+def train_code_ran():
+  with vAR_st.echo():
+    def training():
+
+      #training data
+      training_data = df_training 
+      training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
+            
+      #feature selection for training
+      training_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+      #Label for Training
+      training_data_label = training_data[['Churn']]
+
+      #model training 
+      model = RandomForestClassifier()
+      model_training = model.fit(training_data_features,training_data_label)
+
+
+def test_code_dec():
+  with vAR_st.echo():
+    #training dataset
+    training_data = df_training 
+    training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
+            
+    #feature selection for training
+    training_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+    #Label for Training
+    training_data_label = training_data[['Churn']]
+
+    #model training 
+    model = DecisionTreeClassifier()
+    model_training = model.fit(training_data_features,training_data_label)
+
+    #Test Dataset
+    test_data = df_testing
+
+    #Feature Selection for Model Testing
+    test_data_features = test_data[['Customer Lifetime(Days)']]
+    test_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+    #Model Testing
+    model_testing = model.predict(test_data_features)
+    model_prediction = pd.DataFrame(model_testing)
+    model_prediction = pd.DataFrame(model_testing,columns=['Churn Prediction'])
+
+    prediction_result = test_data.merge(model_prediction,left_index=True,right_index=True)
+
+    #Getting the Probability of Churn
+    prediction_result_probability_all_features = model.predict_proba(test_data_features)
+    prediction_result_probability_all_features = pd.DataFrame(prediction_result_probability_all_features,
+      columns=['Probability of Non Churn', 'Probability of Churn'])
+    churn_probability = prediction_result.merge(prediction_result_probability_all_features,
+      left_index=True,right_index=True)
+    table_7 = HTML(churn_probability.to_html(col_space=None,max_rows=10,max_cols=6))
+    vAR_st.write(table_7)
+
+
+def train_code_dec():
+  with vAR_st.echo():
+    def training():
+
+      #training data
+      training_data = df_training 
+      training_data_features = training_data[['Quantity','Price','Service Call','Service Failure Rate%','Customer Lifetime(Days)']]
+            
+      #feature selection for training
+      training_data_features = training_data_features[['Customer Lifetime(Days)']]
+
+      #Label for Training
+      training_data_label = training_data[['Churn']]
+
+      #model training 
+      model = DecisionTreeClassifier()
+      model_training = model.fit(training_data_features,training_data_label)
+
+
+def download(method):
 
   #training dataset
   training_data = df_training 
@@ -180,7 +329,7 @@ def download():
   training_data_label = training_data[['Churn']]
 
   #model training 
-  model = LogisticRegression()
+  model = method()
   model_training = model.fit(training_data_features,training_data_label)
 
   #Test Dataset
@@ -208,8 +357,8 @@ def download():
   csvfile = churn_probability.to_csv()
   b64 = base64.b64encode(csvfile.encode()).decode()
   vAR_filename = "new_text_file_{}_.csv".format(timestr)
-  #vAR_st.markdown("#### Download File ###")
-  href = f'<a href="data:file/csv;base64,{b64}" download="{vAR_filename}">Download File!!</a>'
+  vAR_st.markdown("#### Download File ###")
+  href = f'<a href="data:file/csv;base64,{b64}" download="{vAR_filename}">Download!!</a>'
   vAR_st.markdown(href,unsafe_allow_html=True)
   
 
@@ -257,10 +406,6 @@ with col2:
       vAR_st.write('')
       vAR_st.write('')
       vAR_st.subheader("Model Selection")
-with col3:
-  if vAR_problem != '':
-    if vAR_type != '':
-      vAR_model = vAR_st.selectbox('',('','Decision Tree','Random Forest','Logistics Regression','Linear Regression','K Means Clustering'),index=0)
 with col5:
   if vAR_problem != '':
     if vAR_type != '':
@@ -268,6 +413,48 @@ with col5:
       vAR_st.write('')
       model_selection_source_code = vAR_st.button('source code',key='13')
 
+with col3:
+  if vAR_problem != '':
+    if vAR_type != '':
+      vAR_model = vAR_st.selectbox('',('','Decision Tree','Random Forest','Logistic Regression','Linear Regression','K Means Clustering'),index=0)
+      if model_selection_source_code:
+        if vAR_model == 'Decision Tree':
+          with vAR_st.echo():
+            if vAR_model == 'Decision Tree':
+              import pandas as pd
+              from sklearn.tree import DecisionTreeClassifier
+        elif vAR_model == 'Random Forest':
+          with vAR_st.echo():
+            if vAR_model == 'Random Forest':
+              import pandas as pd
+              from sklearn.ensemble import RandomForestClassifier
+        elif vAR_model == 'Logistic Regression':
+          with vAR_st.echo():
+            if vAR_model == 'Logistic Regression':
+              import pandas as pd
+              from sklearn.linear_model import LogisticRegression
+        elif vAR_model == 'Linear Regression':
+          with vAR_st.echo():
+            if vAR_model == 'Linear Regression':
+              import pandas as pd
+              from sklearn.linear_model import LinearRegression
+        elif vAR_model == 'K Means Clustering':
+          with vAR_st.echo():
+            if vAR_model == 'K Means Clustering':
+              import pandas as pd
+              from sklearn.cluster import KMeans
+
+      else:
+        if vAR_model == 'Decision Tree':
+          from sklearn.tree import DecisionTreeClassifier
+        elif vAR_model == 'Random Forest':
+          from sklearn.ensemble import RandomForestClassifier
+        elif vAR_model == 'Logistic Regression':
+          from sklearn.linear_model import LogisticRegression
+        elif vAR_model == 'Linear Regression':
+          from sklearn.linear_model import LinearRegression
+        elif vAR_model == 'K Means Clustering':
+          from sklearn.cluster import KMeans
 
 
 vAR_st.write('')
@@ -356,7 +543,7 @@ with col5:
 
 
 
-col1, col2, col3, col4, col5= vAR_st.columns([0.25,2,2,2,1.75])
+col1, col2, col3, col4, col5= vAR_st.columns([0.25,1.5,3.5,4.5,1.75])
 with col3:
   if vAR_problem != '':
     if vAR_type != '':
@@ -400,7 +587,15 @@ with col3:
           button_train = vAR_st.button('Train the Model')
           if button_train:
             vAR_st.image('https://i.gifer.com/IPNp.gif',width = 200)
-            training()
+            if vAR_model == "Logistic Regression":
+              method = LogisticRegression
+              training(method)
+            elif vAR_model == "Random Forest":
+              method = RandomForestClassifier
+              training(method)
+            elif vAR_model == "Decision Tree":
+              method = DecisionTreeClassifier
+              training(method)
             vAR_success = vAR_st.success('Model training completed')
 with col5:
   if vAR_problem != '':
@@ -418,7 +613,12 @@ if vAR_problem != '':
     if vAR_model != '':
       if vAR_training_data:
         if train_source_code:
-          train_code()
+          if vAR_model == "Logistic Regression":
+            train_code_log()
+          elif vAR_model == "Random Forest":
+            train_code_ran()
+          elif vAR_model == "Decision Tree":
+            train_code_dec()
 
 
 
@@ -526,7 +726,12 @@ if vAR_problem != '':
         if vAR_testing_data is not None:
           if vAR_testing_data.type == 'application/vnd.ms-excel' or 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
             if test_source_code:
-              test_code()
+              if vAR_model == "Logistic Regression":
+                test_code_log()
+              elif vAR_model == "Random Forest":
+                test_code_ran()
+              elif vAR_model == "Decision Tree":
+                test_code_dec()
 
 
 
@@ -539,7 +744,17 @@ with col2:
           if vAR_testing_data is not None:
             if vAR_testing_data.type == 'application/vnd.ms-excel' or 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
               if button_test:
-                testing()
+                if vAR_model == "Logistic Regression":
+                  method = LogisticRegression
+                  testing(method)
+                elif vAR_model == "Random Forest":
+                  method = RandomForestClassifier
+                  testing(method)
+                elif vAR_model == "Decision Tree":
+                  method = DecisionTreeClassifier
+                  testing(method)
+
+
 vAR_st.markdown('#')
 vAR_st.markdown("""
 <hr style="width:100%;height:3px;background-color:gray">
@@ -558,7 +773,7 @@ if choice == "Model Validation":
 
 if choice == "Download Model Outcome":
   vAR_st.subheader("To Download the Model Outcome")
-  col1, col2, col4 = vAR_st.columns([3,4,4])
+  col1, col2, col4 = vAR_st.columns([2,4,4])
   with col1:
     if vAR_problem != '':
       if vAR_type != '':
@@ -568,7 +783,15 @@ if choice == "Download Model Outcome":
               if vAR_testing_data.type == 'application/vnd.ms-excel' or 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
                 button_download = vAR_st.button("Click here", key="9")
                 if button_download:
-                  download()
+                  if vAR_model == "Logistic Regression":
+                    method = LogisticRegression
+                    download(method)
+                  elif vAR_model == "Random Forest":
+                    method = RandomForestClassifier
+                    download(method)
+                  elif vAR_model == "Decision Tree":
+                    method = DecisionTreeClassifier
+                    download(method)
                 
 
 if choice == "Data visualization":
@@ -589,12 +812,6 @@ lib = vAR_st.sidebar.selectbox(" ",library)
 services = ["GCP Services Used","VM Instance","Compute Engine"]
 gcp = vAR_st.sidebar.selectbox(" ",services)
 
-vAR_st.write('')
-vAR_st.write('')
-vAR_st.write('')
-from bokeh.models.widgets import Div
-if vAR_st.sidebar.button('Clear/Refresh'):
-  js = "window.location.href = 'http://localhost:8501/'"
-  html = '<img src onerror="{}">'.format(js)
-  div = Div(text=html)
-  vAR_st.bokeh_chart(div)
+
+href = f'<a style="color:black;" href="http://localhost:8501/" class="button">Clear/Reset</a>'
+vAR_st.sidebar.markdown(href, unsafe_allow_html=True)
